10b7a44aa408e7d6a4577ab172806dcde97c9ee8
ram <ramjawade.dev@gmail.com>
2025-09-27 22:38:59 +0530

    remove SSR completely - pure static Angular app for GitHub Pages

--- projects/habit-buddy/src/app/app.config.ts
+++ projects/habit-buddy/src/app/app.config.ts
@@ -2,13 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withHashLocation()), 
-    provideClientHydration(withEventReplay())
+    provideRouter(routes, withHashLocation())
   ]
 };
